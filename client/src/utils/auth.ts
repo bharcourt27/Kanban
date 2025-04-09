6b603dc85fa4b6cf50d6ed7211544e75d60a112a
Brad Harcourt <bharcourt27@gmail.com>
2025-04-09 13:53:44 -0400

    fixed env file

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,50 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
+  }
+
+  loggedIn() {
+    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
+  }
+  
+  isTokenExpired(token: string) {
+    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded.exp) {
+        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+        return decoded.exp < currentTime;
+      }
+      return false; // If no expiration time, assume it's not expired
+    } catch (error) {
+      return true; // If decoding fails, treat the token as expired
+    }
+  }
+
+  getToken(): string {
+    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
+  }
+
+  login(idToken: string) {
+    // TODO: set the token to localStorage
+    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // TODO: remove the token from localStorage
+    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login');
+  }
+}
+
+export default new AuthService();
