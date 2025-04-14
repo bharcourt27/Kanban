3cf767f4560af3cf3c891de7801378a95cc5cc44
Brad Harcourt <bharcourt27@gmail.com>
2025-04-14 13:45:12 -0400

    fixed page reload error

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,15 +6,23 @@ dotenv.config();
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import path from 'node:path';
+import type { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
-
 app.use(express.json());
 app.use(routes);
+
+// Serves static files in the entire client's dist folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
